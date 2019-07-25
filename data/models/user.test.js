05fe87d234c8f1f1cd4405749a51e02758fa5da8
Orie Chinedu <oriechinedu@gmail.com>
2019-07-25 13:29:04 +0100

    test for successful insertion into the db

--- data/models/user.test.js
+++ data/models/user.test.js
@@ -1,12 +1,33 @@
-const User = require('./dbConfig')
+const User = require("./user");
 
 beforeAll(async () => {
-  await db('users').truncate();
+
+    await db("users").truncate();
+
 });
 
-describe('User Model', () => {
-  it('returns empty array initially', async () => {
-    const users = await User.getAllUsers();
-    expect(users).toBe([]);
-  });
+describe("User Model", () => {
+
+    it("returns empty array initially", async () => {
+
+        const users = await User.getAllUsers();
+
+        expect(users).toBe([]);
+
+    });
+
+    it("returns empty array initially", async () => {
+
+        const userData = {
+            "username": "ned",
+            "password": "hjdgjghklhlkjjlkds",
+            "department": "ICT"
+        };
+        const [id] = await User.create(userData);
+        const newUser = await User.find({id});
+
+        expect(newUser).toHaveProperty("username", "ned");
+
+    });
+
 });
